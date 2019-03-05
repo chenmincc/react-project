5b21bc406a91c95d3f8da6df432430f0870169bd
Your Name <141213@qq.com>
2019-03-05 15:24:04 +0800

    footer&剩余的详情

--- src/components/details/Particular.js
+++ src/components/details/Particular.js
@@ -8,8 +8,8 @@ class Particular extends React.Component {
         <section className="ImgBox">
            {/* 图片盒子 */}
            <article className="neiBox">
-              <img src="http://p0.jmstatic.com/banner/area/000/000/029.jpg" className="logoJs" />
-              <img src="http://mp5.jmstatic.com/product/003/426/3426004_std/3426004_1000_1000.jpg?v=1490789863&imageView2/2/w/800/q/90" />
+              <img src="http://p0.jmstatic.com/banner/area/000/000/029.jpg" className="logoJs" alt="网络连接失败" />
+              <img src="http://mp5.jmstatic.com/product/003/426/3426004_std/3426004_1000_1000.jpg?v=1490789863&imageView2/2/w/800/q/90"  alt="网络连接失败"/>
            </article>
          </section>
         {/* 商品售价及其他信息 */}
@@ -38,6 +38,7 @@ class Particular extends React.Component {
             碧柔含水清爽保湿防晒乳液新版 50g，让你不怕晒！
           </div>
         </article>
+        <div className="additional-Box">
         {/* 促销 */}
         <article className="additional-info">
           <div className="promotion-wrap">
@@ -48,10 +49,10 @@ class Particular extends React.Component {
                   <span className="item-list-icon">满赠</span>
                   <span className="item-list-text">[已赠完]部分产品满299赠SACRUM ALGAE 保湿水</span>
                 </p>
+                <div className="iconfont icon-youjiantou arrow-icon-down"></div>
               </div>
             </div>
           </div>
-          <div></div>
         </article>
         {/* 运费 */}
         <article className="additional-list">
@@ -63,6 +64,34 @@ class Particular extends React.Component {
             </div>
           </div>
         </article>
+        {/* 说明 */}
+        <article className="introduction-wrap">
+          <div className="introductions-content">
+            <div className="introduction-type">说明</div>
+            <div className="introduction-item">
+              <p className="introduction-list-title">
+                <span className="instruction-title-icon iconfont icon-gou"></span>
+                <span className="instruction-title-text">自营保税仓发货</span>
+              </p>
+              <p className="introduction-list-title">
+                <span className="instruction-title-icon iconfont icon-gou"></span>
+                <span className="instruction-title-text">24小时内发货</span>
+              </p>
+              <p className="introduction-list-title">
+                <span className="instruction-title-icon iconfont icon-gou"></span>
+                <span className="instruction-title-text">7天拆封无条件退货</span>
+              </p>
+              <p className="introduction-list-title">
+                <span className="instruction-title-icon iconfont icon-gou"></span>
+                <span className="instruction-title-text">支持分期</span>
+              </p>
+            <div className="iconfont icon-youjiantou allSelect"></div>
+            </div>
+          </div>
+        </article>
+        </div>
+        {/* 评论logo */}
+
       </div>
     )
   }
---
+++ src/components/details/commonModule/footer.js
@@ -0,0 +1,29 @@
+/**
+ * 2019.3.5 - Ensol - write in
+ * Move out
+ */
+
+import React from 'react';
+import '@/components/details/Less/footer.less';
+
+class Footer extends React.Component {
+  render () {
+    return (
+      <div className="footer_box">
+        <div className="gwc_left">
+          <div className="boxFlex">
+            <span className="iconfont icon-gouwuche" id="shopping-cart"></span>
+            <span className="textCart">购物车</span>
+          </div>
+        </div>
+        <div className="gm_right">
+          <a href="@" className="jr_gwc">加入购物车</a>
+          <a href="@" className="lj_gm">立即购买</a>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Footer;
+
