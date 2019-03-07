3b33159b1456ce0f9b1e60600540e26f7e21122f
qi <zheng@example.com>
2019-03-07 20:13:03 +0800

    脚部高亮

--- src/index.js
+++ src/index.js
@@ -3,18 +3,24 @@ import ReactDOM from 'react-dom';
 import '@/style/reset.less';
 import {HashRouter, Route, Switch} from 'react-router-dom';
 import Home from '@/components/Home.js';
+import Footer from '@/components/total/index.js';
 import Particular from '@/components/details/Particular.js';
 import Register from '@/components/register/index.js';
+import Login from '@/components/login/index.js';
 
 class App extends React.Component {
   render () {
     return (
       <HashRouter>
+        <>
          <Switch>
           <Route path="/detail/:itemId" component={Particular}></Route>
           <Route path="/register" component={Register}></Route>
+          <Route path="/login" component={Login}></Route>          
           <Route path="/" component={Home}></Route>
         </Switch>
+        <Footer />
+        </>
       </HashRouter>
     )
   }
